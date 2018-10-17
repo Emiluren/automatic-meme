9e426d36fac327ec46f7cca952f9272f4ecaa14e
Emil Segerbäck <emilsegerback@gmail.com>
2018-10-17 18:27:08 +0200

    Add more text about convergence

--- project2/mysol.m
+++ project2/mysol.m
@@ -1,7 +1,7 @@
 function [root] = mysol(func, x0, x1, accuracy)
 korr = ((x1 - x0)* func(x1)/(func(x1) - func(x0)));
 if abs(korr) > accuracy
-    root = (mysol(func, x1, x1-korr, accuracy));
+    root = mysol(func, x1, x1-korr, accuracy);
 else
     root = x1 - korr;
 end
