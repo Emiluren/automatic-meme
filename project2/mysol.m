6b881a90fd5186040c669bbd0dff53ff819ca11b
Emil Segerbäck <emilsegerback@gmail.com>
2018-10-17 13:22:59 +0200

    Fix project 2 solution and move it to project 2 folder

--- project2/mysol.m
+++ project2/mysol.m
@@ -1,6 +1,6 @@
 function [root] = mysol(func, x0, x1, accuracy)
 korr = ((x1 - x0)* func(x1)/(func(x1) - func(x0)));
-if korr > accuracy
+if abs(korr) > accuracy
     root = (mysol(func, x1, x1-korr, accuracy));
 else
     root = x1 - korr;
