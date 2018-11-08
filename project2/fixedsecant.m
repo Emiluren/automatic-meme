60a582c85df85ed3124814d499b57f6d3b4c986e
Emil Segerbäck <emilsegerback@gmail.com>
2018-11-08 23:47:17 +0100

    Fix last convergence table

--- project2/fixedsecant.m
+++ project2/fixedsecant.m
@@ -10,3 +10,16 @@ function [xs] = fixedsecant(func, x0, x1, n)
     xs(i + 2) = x1;
   end
 end
+
+% Polynom
+xs = fixedsecant(@(x) (x+2)*x*(x-3), -1.5, -2.5, 10);
+errors = abs(xs(2:11) - xs(1:10))
+logs = log(errors(2:10) ./ errors(1:9))
+ps = logs(2:8) ./ logs(1:7)
+
+% Dubbelrot
+results = fixedsecant(@(x) x*x, 1.5, 1.55, 520);
+xs = results(500:520)
+errors = abs(xs(2:21) - xs(1:20))
+logs = log(errors(2:19) ./ errors(1:18))
+ps = logs(2:18) ./ logs(1:17)
